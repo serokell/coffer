dfa890af6c756cb1c167e2831d14e59333525ec3
Leonid Vasilev <leonid.vasilev@serokell.io>
2022-03-22 13:28:28 +0300

    [#43] `run-bats-tests` script added.
    Problem: in order to run `bats` tests you need to manually start `vault` and then run `make bats`.
    Solution: added script that starts two instances of `vault`, runs `bats` tests
    and after all kills these two instances.

---
+++ scripts/run-bats-tests.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+# SPDX-FileCopyrightText: 2022 Serokell <https://serokell.io>
+#
+# SPDX-License-Identifier: MPL-2.0
+
+# This script updates bats git submodules, runs two vault instances,
+# then runs bats tests and after all kills vault instances.
+
+git submodule update --init --recursive
+
+FILTER=$1
+
+vault server -dev -dev-root-token-id="root" -dev-listen-address="localhost:8209" > /dev/null 2>&1 &
+vault1_pid=$!
+
+vault server -dev -dev-root-token-id="second" -dev-listen-address="localhost:8210" > /dev/null 2>&1 &
+vault2_pid=$!
+
+if [ -z "$FILTER" ]; then
+  ./tests/golden/helpers/bats/bin/bats ./tests/golden/**
+else
+  ./tests/golden/helpers/bats/bin/bats ./tests/golden/** -f "$FILTER"
+fi
+
+kill $vault1_pid > /dev/null 2>&1
+kill $vault2_pid > /dev/null 2>&1
