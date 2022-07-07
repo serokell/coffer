b124dfb31bc03667e61a9d8d98d9acba56d0f2e7
Kirill Antonov <kirillant2013@gmail.com>
2022-07-07 20:49:40 +0300

    [#95] Added `BackendName` FromJSON instance
    Problem:
    Goal is to add `VaultKvBackend` `FromJSON` instance
    
    Solution:
    Add instance to `BackendName`

--- lib/BackendName.hs
+++ lib/BackendName.hs
@@ -11,6 +11,7 @@ module BackendName
   ) where
 
 import Coffer.Util (didimatch)
+import Data.Aeson qualified as A
 import Data.Hashable (Hashable)
 import Data.Text (Text)
 import Data.Text qualified as T
@@ -20,6 +21,12 @@ import Toml qualified
 newtype BackendName = UnsafeBackendName Text
   deriving stock (Show, Eq)
   deriving newtype (Hashable, Buildable)
+
+instance A.FromJSON BackendName where
+  parseJSON = A.withText "BackendName" \text ->
+    case newBackendName text of
+      Right fieldName -> return fieldName
+      Left err -> fail $ T.unpack err
 
 backendNameCharSet :: [Char]
 backendNameCharSet = ['a'..'z'] ++ ['A'..'Z'] ++ ['0'..'9'] ++ "-_;"
